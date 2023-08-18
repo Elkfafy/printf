01c265ebb887e6d987bb3b9f5b4120591cd0ae28
elkfafy <hmdeslam.adel@gmail.com>
2023-08-18 15:34:01 +0300

    init

---
+++ test/0-main.c
@@ -0,0 +1,14 @@
+#include "../main.h"
+/**
+ * main - check task 0 (check if printf return correct output from numbers)
+ * need also to handle c - s - % specifiers
+ * Return: 0
+ */
+int main(void)
+{
+	int pridicted = _printf("%s%c", "This is for testing", '\n');
+	int real = printf("%s%c", "This is for testing", '\n');
+
+	printf("pridicted = %d, real =d\n", pridicted, real);
+	return (0);
+}
