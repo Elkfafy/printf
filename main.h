4c281b45e919db2bbc5097e90f9e28cad613974f
elkfafy <hmdeslam.adel@gmail.com>
2023-08-18 21:34:36 +0300

    init

--- main.h
+++ main.h
@@ -4,5 +4,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 int _putchar(char);
+int print_str(char *);
+int print_int(int);
+int get_int_length(int);
+char *int_to_str(int);
+int get_str_length(char *);
+int get_arr_size(void *, unsigned int);
+int get_str_length(char *);
 
 #endif
